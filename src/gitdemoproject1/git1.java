f769aa048a83d9f15adf2b0cbc12c33d7d506aed
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-08-17 10:12:33 +0530

    first commit.....

---
+++ src/gitdemoproject1/git1.java
@@ -0,0 +1,15 @@
+package gitdemoproject1;
+
+public class git1 {
+	public static void main(String[] args)
+	{
+	String humgry = "Yes";
+	if(humgry == "Yes")
+		System.out.println("eat something...");
+	else {
+		System.out.println("Do work...");
+
+}
+}
+}
+
